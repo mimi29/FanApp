271800ab3153cf7a7f3d8cf667f6b32895025fd7
unknown <ming29398@gmail.com>
2019-02-06 05:12:47 -0800

    added preference

--- models/memberpreference.js
+++ models/memberpreference.js
@@ -8,10 +8,10 @@ const MemberPreferenceSchema = new Schema( {
 		passwd: {type: String, required: true}
 	},  
 	prefs: {
-		// hotel_pref: {type: String},
-		// activity_pref: {type: String},
-		// restaurant_pref: {type: String},
-	    all_pref: [Number]
+		hotel_pref: {type: String},
+		activity_pref: {type: String},
+		restaurant_pref: {type: String},
+	    //all_pref: [Number]
 
 	},
 	id:{
